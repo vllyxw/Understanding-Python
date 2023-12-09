f0b33e7ab77045015f727fb72823501a25289673
vllyxw <victoriapansini661@gmail.com>
2023-12-08 22:30:10 -0300

    main

---
+++ 1. FUNDAMENTOS DE PYTHON/CÓDIGOS/estruturas_condicionais.py
@@ -0,0 +1,10 @@
+MAIOR_IDADE = 18
+IDADE_ESPECIAL = 12
+
+idade = int(input("Informe sua idade: "))
+
+if idade >= MAIOR_IDADE:
+    print("Maior de idade, pode tirar a CNH!")
+else:
+    print("Você é ainda menor de idade, terá que esperar")
+    
